c0109e3c814ce68a932d3937d704a73654c35269
Justin Houk <jstndhouk@gmail.com>
2021-11-11 22:25:04 -0500

    same as previous commit

--- db/schema.sql
+++ db/schema.sql
@@ -12,9 +12,8 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary VARCHAR(30) NOT NULL,
-  department_id VARCHAR(30) NOT NULL, 
-  FOREIGN KEY (department_id),
-  REFERENCES department(id)
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -22,7 +21,6 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NOT NULL
-  FOREIGN KEY (role_id),
-  REFERENCES role(id)
+  manager_id INT DEFAULT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,30 +5,30 @@ VALUES ('Human Resources'),
        ("Estimating");
        
 INSERT INTO role (title, salary, department_id)
-VALUES (Project Manager, '110000', '3'),
-       (Director, '300000', '3'),
-       (Project Engineer, '70000', '3'),
-       (Lawyer, '100000', '2'),
-       (File Clerk, '60000', '2' )
-       (Estimator, '80000', '4'),
-       (Senior Estimator, '100000', '4');
-       (Payroll Specialist, '60000', '1');
-       (Benefits Specialist, '50000', '1');
+VALUES ('Project Manager', '110000', '3'),
+       ('Director', '300000', '3'),
+       ('Project Engineer', '70000', '3'),
+       ('Lawyer', '100000', '2'),
+       ('Clerk', '60000', '2' ),
+       ('Estimator', '80000', '4'),
+       ('Senior Estimator', '100000', '4'),
+       ('Payroll Specialist', '60000', '1'),
+       ('Benefits Specialist', '50000', '1');
        
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Shinida', 'Bump', '1'),
-       ('Sharon', 'Needles', '1'),
-       ('Trixie', 'Mattel', '5'),
-       ('Katya', 'Zamalochakova', '5'),
-       ('Ginger', 'Minj','4');
-       ('Violet', 'Chachki', '4');
-       ('Bianca', 'DelRio','2');
-       ('Alexis', 'Matteo','3');
-       ('Crystall', 'Method', '3');
-       ('Aleska', 'ThunderF**k5000', '6');
-       ('Trinity', 'The Tuck', '6');
-       ('Alexis', 'Bevels', '7');
-       ('Darby', 'Carhart', '7');
-       ('Auntie', 'Chan', '8');
-       ('Tammy', 'Brown', '9');
+VALUES ('Shinida', 'Bump', '1', '2'),
+       ('Sharon', 'Needles', '1', '2'),
+       ('Trixie', 'Mattel', '5', '4'),
+       ('Katya', 'Zamalochakova', '5','4'),
+       ('Ginger', 'Minj','4', NULL),
+       ('Violet', 'Chachki', '4', NULL),
+       ('Bianca', 'DelRio','2', NULL),
+       ('Alexis', 'Matteo','3', '2'),
+       ('Crystall', 'Method', '3', '2'),
+       ('Aleska', 'ThunderF**k5000', '6', '7'),
+       ('Trinity', 'The Tuck', '6', '7'),
+       ('Alexis', 'Bevels', '7', NULL),
+       ('Darby', 'Carhart', '7', NULL),
+       ('Auntie', 'Chan', '8', NULL),
+       ('Tammy', 'Brown', '9', NULL);
        
