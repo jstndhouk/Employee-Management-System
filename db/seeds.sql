bef7fd142ad067c5399ee0eebfe10ae27e4651fc
Justin Houk <jstndhouk@gmail.com>
2021-11-11 21:51:05 -0500

    created the seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,34 @@
-INSERT INTO products (id, product_name, category_name)
-VALUES (001, "spinach", "produce"),
-       (002, "peanut butter", "staples"),
-       (003, "peas-canned", "canned goods"),
-       (004, "ice cream", "frozen"),
-       (005, "potato chips", "snacks");
+INSERT INTO department (name)
+VALUES ('Human Resources'),
+       ("Legal"),
+       ("Operations"),
+       ("Estimating");
+       
+INSERT INTO role (title, salary, department_id)
+VALUES (Project Manager, '110000', '3'),
+       (Director, '300000', '3'),
+       (Project Engineer, '70000', '3'),
+       (Lawyer, '100000', '2'),
+       (File Clerk, '60000', '2' )
+       (Estimator, '80000', '4'),
+       (Senior Estimator, '100000', '4');
+       (Payroll Specialist, '60000', '1');
+       (Benefits Specialist, '50000', '1');
+       
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Shinida', 'Bump', '1'),
+       ('Sharon', 'Needles', '1'),
+       ('Trixie', 'Mattel', '5'),
+       ('Katya', 'Zamalochakova', '5'),
+       ('Ginger', 'Minj','4');
+       ('Violet', 'Chachki', '4');
+       ('Bianca', 'DelRio','2');
+       ('Alexis', 'Matteo','3');
+       ('Crystall', 'Method', '3');
+       ('Aleska', 'ThunderF**k5000', '6');
+       ('Trinity', 'The Tuck', '6');
+       ('Alexis', 'Bevels', '7');
+       ('Darby', 'Carhart', '7');
+       ('Auntie', 'Chan', '8');
+       ('Tammy', 'Brown', '9');
        
